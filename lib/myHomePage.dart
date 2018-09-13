b0b1543796c2a41cfff57d8fa09d5ac60faff739
鳥越 花奈美 <>
2018-09-13 22:19:20 +0900

    question&percent

--- lib/myHomePage.dart
+++ lib/myHomePage.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import 'login.dart';
 
-
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  
   final String title;
 
   @override
@@ -14,53 +12,82 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  List<Map> _choiceData = [
+    {
+      "question": "好きな動物は何ですか",
+      "answer1": "犬",
+      "answer1count": 10,
+      "answer1percent1": 33,
+      "answer2": "猫",
+      "answercount2": 20,
+      "answer1percent2": 66,
+      "total": 30,
+    },
+  ];
 
-  void _incrementCounter() {
-    setState(() {
-     
-      _counter++;
-    });
-  }
-  
   void _gotologinpage() {
     // print("jhk");
     Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => Loginpage(title: 'Flutter')),
-            );
+      context,
+      MaterialPageRoute(builder: (context) => Loginpage(title: 'Flutter')),
+    );
+  }
 
+  Widget _buildListItem(context, choiceItem) {
+    print(choiceItem);
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(choiceItem["question"]),
+          Container(
+            height: 8.0,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              RaisedButton(
+                onPressed: () {},
+                child: Text(choiceItem["answer1"]),
+              ),
+              RaisedButton(
+                onPressed: () {},
+                child: Text(choiceItem["answer2"]),
+              ),
+            ],
+          ),
+          Container(
+            color: Colors.blue,
+            child: Text(choiceItem["answer1count"].toString()),
+            width: (MediaQuery.of(context).size.width - 16.0) * choiceItem["answer1percent1"]/100,
+          ),
+          Container(
+            color: Colors.amber,
+            child: Text(
+              choiceItem["answercount2"].toString()),
+            width: (MediaQuery.of(context).size.width - 16.0) * choiceItem["answer1percent2"]/100,
+            ),
+        ],
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    
-
-
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        
-        child: Column(
-          
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
+      body: ListView(
+          children: List<Widget>.generate(_choiceData.length, (index) {
+        return _buildListItem(context, _choiceData[index]);
+      })),
       floatingActionButton: FloatingActionButton(
         onPressed: _gotologinpage,
         tooltip: 'Increment',
         child: Icon(Icons.add),
-      ), 
+      ),
     );
   }
 }
